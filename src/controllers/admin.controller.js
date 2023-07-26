9e9f617a45968a84c8715a99e8a22ed03d573e8f
anand <anand.prasad@appinventiv.com>
2023-07-26 16:08:45 +0530

    admin api 2 done

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -5,6 +5,9 @@ module.exports.adminController = {
     getBestProfession: async (req, res) => {
         try {
             const { start, end } = req.query;
+            if (!start || !end) {
+                return res.status(400).json({ message: 'start/end is required ' })
+            }
             const { Contract, Profile, Job } = req.app.get('models');
             let data = await Job.findOne({
                 where: {
@@ -43,12 +46,60 @@ module.exports.adminController = {
                 profession: 'N/A'
             }
             if (data != null) {
-                response = {
-                    total: data['total_amount'],
-                    profession: data['Contract.Contractor.profession']
-                }
+                response.total = data['total_amount'];
+                response.profession = data['Contract.Contractor.profession'];
             }
             return res.json({ response });
+        } catch (error) {
+            return res.status(500).json({
+                message: error.message || error
+            })
+        }
+    },
+
+    getBestClients: async (req, res) => {
+        try {
+            let { start, end, limit } = req.query;
+            const { Profile, Job, Contract } = req.app.get('models');
+            if (!start || !end) return res.status(400).json({ message: 'start/end is required ' })
+            if (!limit) limit = 2;
+
+            const data = await Job.findAll({
+                where: {
+                    paid: true,
+                    [Op.and]: [
+                        { createdAt: { [Op.gte]: new Date(start) } },
+                        { createdAt: { [Op.lte]: new Date(end) } }
+                    ]
+                },
+                include: {
+                    model: Contract,
+                    include: {
+                        model: Profile,
+                        as: 'Client',
+                        attributes: [
+                            // 'id'
+                        ]
+                    },
+                    attributes: [
+                        ['ClientId', 'clientId']
+                    ]
+                    // group: ['ContractId']
+                },
+                raw: true,
+                attributes: [
+                    [sequelize.literal("firstName || ' ' || lastName"), 'fullName'],
+                    [sequelize.fn('sum', sequelize.col('price')), 'paid'],
+                ],
+                group: [
+                    [sequelize.literal("firstName || ' ' || lastName"), 'fullName']
+                ],
+                order: [
+                    ['paid', 'DESC']
+                ],
+                limit: +limit
+            });
+            return res.json({ limit: +limit, data });
         } catch (error) {
             return res.status(500).json({
                 message: error.message || error
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { adminController: controller } = require('../controllers/admin.controller');
 
-router.get('/best-profession', controller.getBestProfession)
+router.get('/best-profession', controller.getBestProfession);
+router.get('/best-clients', controller.getBestClients);
 
 module.exports = router;
