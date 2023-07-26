9de5ca73396e92b39112106fbfdd30b2b69e4a76
anand <anand.prasad@appinventiv.com>
2023-07-26 12:02:21 +0530

    job transaction doen

--- src/controllers/job.controller.js
+++ src/controllers/job.controller.js
@@ -1,4 +1,6 @@
+const { sequelize } = require("../model");
 const { Op } = require("sequelize");
+
 
 
 
@@ -36,9 +38,84 @@ module.exports.jobController = {
         }
     },
 
-    payForAJob: async(req, res) => {
+    payForAJob: async (req, res) => {
         try {
-            
+            const { Job, Contract, Profile } = req.app.get('models');
+            const jobDetails = await Job.findOne({
+                where: {
+                    id: req.params.job_id
+                },
+                attributes: [
+                    "id",
+                    "description",
+                    "price",
+                    "paid",
+                    "paymentDate",
+                    "createdAt",
+                    "updatedAt",
+                ],
+                include: {
+                    model: Contract,
+                    attributes: [
+                        "id",
+                        "terms",
+                        "status",
+                        "createdAt",
+                        "updatedAt",
+                        "ClientId",
+                    ],
+                    include: {
+                        model: Profile,
+                        as: 'Contractor',
+                        attributes: [
+                            "id",
+                            "firstName",
+                            "lastName",
+                            "type",
+                            "balance"
+                        ]
+                    }
+                }
+            });
+            const transaction = await sequelize.transaction();
+            let user = null;
+            try {
+                user = await Profile.findOne({ where: { id: req.profile.id } }, { transaction: transaction, lock: true });
+                if (user.balance < jobDetails.price) return res.status(400).json({ message: "Not enough balance !" });
+                
+                // Debit from source
+                await Profile.update({ balance: user.balance - jobDetails.price }, {
+                    where: {
+                        id: req.profile.id
+                    },
+                    transaction: transaction
+                });
+
+                // Credit to target
+                await Profile.update({ balance: jobDetails.Contract.Contractor.balance + jobDetails.price }, {
+                    where: {
+                        id: jobDetails.Contract.Contractor.id
+                    },
+                    returning: true,
+                    transaction: transaction
+                });
+
+                // Update job status
+                await Job.update({paid: 1, paymentDate: new Date()}, {
+                    where: {
+                        id: req.params.job_id
+                    },
+                    transaction: transaction
+                });
+
+                transaction.commit();
+            } catch (error) {
+                transaction.rollback();
+                console.log("Rolled back. ...");
+                return res.status(500).json({ error: error?.message || error });
+            }
+
+            return res.json({ message: `Successfully paid for your job jobId = ${req.params.job_id} to contractor = ${jobDetails.Contract.Contractor.firstName}!`});
         } catch (error) {
             return res.status(500).json({ error: error?.message || error });
         }
--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -5,6 +5,6 @@ const {jobController: controller} = require('../controllers/job.controller');
 
 
 router.get('/unpaid', getProfile, controller.getMyUnpaidJobs);
-router.post('/:job_id/pay', controller.payForAJob);
+router.post('/:job_id/pay', getProfile, controller.payForAJob);
 
 module.exports = router;
